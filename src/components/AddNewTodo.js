a582c9b417c8e489446908dca1dfa9307feb5b21
Ibro001 <ibrahimmoses56@gmail.com>
2022-07-24 13:20:58 +0100

    created the modal component

--- src/components/AddNewTodo.js
+++ src/components/AddNewTodo.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Modal';
+
 
 const AddNewTodo = () => {
+
+  const [showModal,setShowModal] = useState(false);
+
   return (
-    <div className='AddNewTodo'>
-      AddNewTodo
+    <div className='addNewTodo'>
+      <div className='btn'>
+        <button onClick={() => setShowModal(true)}>
+          + New Todo
+        </button>
+      </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <div>
+          hello world
+          <button onClick={()=> setShowModal(false)}>
+            hide
+          </button>
+        </div>
+      </Modal>
     </div>
   )
 }

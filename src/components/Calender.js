56b9dcf29b4e05c1280851226269a0778edc64fa
Ibro001 <ibrahimmoses56@gmail.com>
2022-07-28 17:36:09 +0100

    Added calender files and styles to the calender component

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import {CalendarDate, CaretUp} from 'react-bootstrap-icons'
+import {calendarItems} from '../constants/index';
+
 
 const Calender = () => {
   return (
     <div className='calender'>
-      Calender
+      <div className='header'>
+        <div className='title'>
+          <CalendarDate 
+            size= "18"
+          />
+          <p>calendar</p>
+        </div>  
+          <div className='btns'>
+            <span>
+              <CaretUp 
+                size='20'
+              />
+            </span>
+          </div>
+      </div>
+      <div className='items'>
+        {
+          calendarItems.map(item => 
+            <div className='item' key={item}>
+              {item}
+            </div>
+            
+          )
+        }
+      </div>
     </div>
   )
 }

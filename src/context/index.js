bc36171364d1323c551c1501ae906ff1c58348a7
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-04 18:24:31 +0100

    created my context folder

---
+++ src/context/index.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react'
+
+const TodoContext = createContext();
+
+const TodoContextProvider = ({children}) => {
+
+    const defaultProject = 'today';
+    const [selectedProject,setSelectedProject] = useState(defaultProject);
+
+    return (
+        <TodoContext.Provider
+            value = {
+                {
+                    selectedProject,
+                    setSelectedProject
+                }
+            }
+        >
+            {children}
+        </TodoContext.Provider>
+    )
+}
+
+export {TodoContextProvider, TodoContext}

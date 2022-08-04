5879dd50376cc33cee3e26451ff0ef6cbc294dd8
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-04 13:23:11 +0100

    Refactored the AddNewTodo component

--- src/components/AddNewTodo.js
+++ src/components/AddNewTodo.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react'
 import Modal from './Modal';
-import {Bell, CalendarDay, Clock, Palette, X} from 'react-bootstrap-icons';
-import { DatePicker, TimePicker,LocalizationProvider } from '@mui/x-date-pickers';
-import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
-import 'date-fns'
-import { TextField } from '@mui/material';
+import TodoForm from './TodoForm';
 
 
 const AddNewTodo = () => {
@@ -14,6 +10,16 @@ const AddNewTodo = () => {
   const [day,setDay] = useState( new Date ());
   const [time,setTime] = useState( new Date ());
 
+  const projects = [
+    {id:1, name: 'personal', numOfTodos: 0},
+    {id:2, name: 'work', numOfTodos: 1},
+    {id:3, name: 'other', numOfTodos: 2}
+  ]
+
+  const handleSubmit = (e) => {
+
+  }
+
   return ( 
     <div className='AddNewTodo'>
             <div className="btn">
@@ -22,70 +28,19 @@ const AddNewTodo = () => {
                 </button>
             </div>
             <Modal showModal={showModal} setShowModal={setShowModal}>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    
-                    <form>
-                        <div className="text">
-                            <h3>Add new to do!</h3>
-                            <input
-                                type='text'
-                                value={text}
-                                onChange={e => setText(e.target.value)}
-                                placeholder='To do ...'
-                                autoFocus
-                            />
-                        </div>
-                        <div className="remind">
-                            <Bell />
-                            <p>Remind Me!</p>
-                        </div>
-                        <div className="pick-day">
-                            <div className="title">
-                                <CalendarDay />
-                                <p>Choose a day</p>
-                            </div>
-                            <DatePicker
-                                renderInput={params=> <TextField {...params} />} 
-                                value={day}
-                                onChange={day => setDay(day)}
-                            />
-                        </div>
-                        <div className="pick-time">
-                            <div className="title">
-                                <Clock />
-                                <p>Choose time</p>
-                            </div>
-                            <TimePicker
-                                renderInput={params => <TextField {...params} />}
-                                value={time}
-                                onChange={time => setTime(time)}
-                            />
-                        </div>
-                        <div className="pick-project">
-                            <div className="title">
-                                <Palette />
-                                <p>Choose a project</p>
-                            </div>
-                            <div className="projects">
-                                <div className="project active">
-                                    personal
-                                </div>
-                                <div className="project">
-                                    work
-                                </div>
-                                <div className="project">
-                                    work
-                                </div>
-                            </div>
-                        </div>
-                        <div className="cancel" onClick={() => setShowModal(false)}>
-                            <X size='40' />
-                        </div>
-                        <div className="confirm">
-                            <button>+ Add to do</button>
-                        </div>
-                    </form>
-                </LocalizationProvider>
+                <TodoForm 
+                    handleSubmit={handleSubmit}
+                    heading= 'Add new todo!'
+                    text={text}
+                    setText={setText}
+                    day={day}
+                    setDay={setDay}
+                    time={time}
+                    setTime={setTime}
+                    projects={projects}
+                    showButton={true}
+                    setShowModal={setShowModal}
+                />
             </Modal>
         </div>
     )

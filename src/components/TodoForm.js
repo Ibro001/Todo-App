ac6cfcdd3d46f31ea51a0172bac6a3e8fe8d818a
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-04 13:26:35 +0100

    created the TodoForm component

---
+++ src/components/TodoForm.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import {Bell, CalendarDay, Clock, Palette, X} from 'react-bootstrap-icons';
+import { DatePicker, TimePicker,LocalizationProvider } from '@mui/x-date-pickers';
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
+import 'date-fns'
+import { TextField } from '@mui/material';
+
+
+const TodoForm = ({
+    handleSubmit,
+    heading = false,
+    text, setText,
+    day, setDay,
+    time, setTime,
+    projects,
+    showButton = false,
+    setShowModal = false
+}) => {
+  return ( 
+    <LocalizationProvider dateAdapter={AdapterDateFns}>           
+        <form onSubmit={handleSubmit} className='todoForm'>
+            <div className="text">
+                {
+                    heading &&
+                    <h3>{heading}</h3>
+                }
+                <input
+                    type='text'
+                    value={text}
+                    onChange={e => setText(e.target.value)}
+                    placeholder='To do ...'
+                    autoFocus
+                />
+            </div>
+            <div className="remind">
+                <Bell />
+                <p>Remind Me!</p>
+            </div>
+            <div className="pick-day">
+                <div className="title">
+                    <CalendarDay />
+                    <p>Choose a day</p>
+                </div>
+                <DatePicker
+                    renderInput={params=> <TextField {...params} />} 
+                    value={day}
+                    onChange={day => setDay(day)}
+                />
+            </div>
+            <div className="pick-time">
+                <div className="title">
+                    <Clock />
+                    <p>Choose time</p>
+                </div>
+                <TimePicker
+                    renderInput={params => <TextField {...params} />}
+                    value={time}
+                    onChange={time => setTime(time)}
+                />
+            </div>
+            <div className="pick-project">
+                <div className="title">
+                    <Palette />
+                    <p>Choose a project</p>
+                </div>
+                <div className="projects">
+                    {
+                        projects.map(project => 
+                            <div className='project' key={project.id}>
+                                {project.name}
+                            </div>   
+                            
+                        )
+                    }
+                </div>
+            </div>
+            {
+                showButton &&
+                <div>
+                    <div className="cancel" onClick={() => setShowModal(false)}>
+                        <X size='40' />
+                    </div>
+                    <div className="confirm">
+                        <button>+ Add to do</button>
+                    </div>
+                </div>
+            }
+        </form>
+</LocalizationProvider>
+        
+    )
+}
+export default TodoForm

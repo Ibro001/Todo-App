6e2873cf6ed1a0c034343b4cd5ad8f721aef486b
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-11 14:18:12 +0100

    created custom  hooks useTodos and useProjects

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,68 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
+import firebase from '../firebase'
+
+export function useTodos(){
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        let unsubscribe = firebase
+        .firestore()
+        .collection('todos')
+        .onSnapshot( snapshot => {
+            const data = snapshot.docs.map( doc => {
+                return {
+                    id : doc.id,
+                    ...doc.data()
+                }
+            })
+            setTodos(data)
+        })
+
+        return () => unsubscribe()
+    }, [])
+
+    return todos
+}
+
+export function useProjects(todos){
+    const [projects, setProjects] = useState([])
+
+    function calculateNumOfTodos(projectName, todos){
+        return todos.filter( todo => todo.projectName === projectName).length
+    }
+
+    useEffect(() => {
+        let unsubscribe = firebase
+        .firestore()
+        .collection('projects')
+        .onSnapshot( snapshot => {
+            const data = snapshot.docs.map( doc => {
+
+                const projectName = doc.data().name
+
+                return {
+                    id : doc.id,
+                    name : projectName,
+                    numOfTodos : calculateNumOfTodos(projectName, todos)
+                }
+            })
+            setProjects(data)
+        })
+
+        return () => unsubscribe()
+    }, [])
+
+    return projects
+}
+
+
+
+
+
+
+
+
+/*import { useState, useEffect} from 'react'
 import { collection, getDocs} from 'firebase/firestore'
 import {db} from '../firebase'
 
@@ -51,7 +115,7 @@ export  function useTodos(){              // custom hook: useTodos//
         return () => {todoCollectionsRef()}
     },[])   //useEffect clean-up to avoid memory leak//
     return todos
-}
+}*/
 
 
 

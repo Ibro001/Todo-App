4c668d89b7f69794eb0f8c9e80cdd38e3c6c7f8a
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-11 14:13:53 +0100

    setup firebase SDK

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,7 +1,6 @@
-import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD67o540riAOvfZ-Ust8-FaNm2weLFApEE",
   authDomain: "todo-app-2fe7a.firebaseapp.com",
@@ -9,11 +8,38 @@ const firebaseConfig = {
   storageBucket: "todo-app-2fe7a.appspot.com",
   messagingSenderId: "746122094294",
   appId: "1:746122094294:web:b84b5998f39e61359f8b77"
-};
+  }
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
+
+
+
+
+
+
+
+
+
+
+
+/*import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+/*const firebaseConfig = {
+  apiKey: "AIzaSyD67o540riAOvfZ-Ust8-FaNm2weLFApEE",
+  authDomain: "todo-app-2fe7a.firebaseapp.com",
+  projectId: "todo-app-2fe7a",
+  storageBucket: "todo-app-2fe7a.appspot.com",
+  messagingSenderId: "746122094294",
+  appId: "1:746122094294:web:b84b5998f39e61359f8b77"
+};*/
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
 //init firestore
-export const db = getFirestore(app)
+//export const db = getFirestore(app)
 
 

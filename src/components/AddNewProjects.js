24b9398804c4b61e98b206a43037e5fc77538499
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-11 14:21:37 +0100

    added functionality to the addNewProject component form

--- src/components/AddNewProjects.js
+++ src/components/AddNewProjects.js
@@ -2,13 +2,36 @@ import React, { useState } from 'react'
 import Modal from './Modal';
 import ProjectForm from './ProjectForm';
 import { Plus} from 'react-bootstrap-icons';
+import firebase from '../firebase'
 
 const AddNewProjects = () => {
-
+  //State
   const [showModal,setShowModal] = useState(false);
   const [projectName,SetProjectName] = useState('');
   const handleSubmit = (e)=>{
+      e.preventDefault()
 
+      if(projectName){
+        const projectsRef = firebase.firestore().collection('projects')
+
+        projectsRef
+          .where('name', '==', projectName)
+          .get()
+          .then(querySnapshot => {
+            if(querySnapshot.empty){
+              projectsRef
+              .add(
+                {
+                  name: projectName
+                }
+              )
+            }else{
+              alert('project already exist!')
+            }
+          })
+          setShowModal(false)
+          SetProjectName('')
+      }
   }
 
   return (

33933617127c2024beb374e322011dc511fa5b62
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-14 10:48:48 +0100

    added functionality to the RenameProject component so i can rename my projects

--- src/components/RenameProject.js
+++ src/components/RenameProject.js
@@ -1,12 +1,63 @@
-import React, { useState }  from 'react';
+import React, { useContext, useState }  from 'react';
 import ProjectForm from './ProjectForm';
+import firebase from '../firebase'
+import { TodoContext } from '../context'
 
 
 const RenameProject = ({project, setShowModal}) => {
+  //State
+  const [newProjectName,setNewProjectName] = useState(project.name);
+  
+  //Context
+  const {selectedProject, setSelectedProject} = useContext(TodoContext)
+  
+  //Rename Project
+  const renameProject = (project, newProjectName) => {
+    const projectsRef = firebase.firestore().collection('projects');
+    const todosRef = firebase.firestore().collection('todos');
 
-  const [newProjectName,setNewProjectName] =useState(project.name);
+    const {name: oldProjectName} = project //destructured my project//
+
+    projectsRef
+    .where('name', '==', newProjectName)
+    .get()
+    .then( querySnapshot => {
+      if(!querySnapshot.empty){
+        alert('Project with thesame name already exist!')
+      }else{
+        projectsRef
+        .doc(project.id)
+        .update({
+          name: newProjectName
+        })
+        .then(() => {
+          todosRef
+          .where('projectName', '==', oldProjectName)
+          .get()
+          .then( querySnapshot => {
+            querySnapshot.forEach(doc => {
+              doc.ref.update({
+                projectName: newProjectName
+              })
+            })
+          })
+          .then(() => {
+            if(selectedProject === oldProjectName){
+              setSelectedProject(newProjectName)
+            }
+          })
+        })
+      }
+    })
+
+  }
+
   const handleSubmit = (e) => {
+    e.preventDefault()
 
+    renameProject(project, newProjectName)
+
+    setShowModal(false)
   }
 
   return (

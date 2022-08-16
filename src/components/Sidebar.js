b17997e0c9baea817dd02754c1eec55da904f30d
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-16 06:53:15 +0100

    set sideBar to listen to event that hides EditTodo ccomponent

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
+import { TodoContext } from '../context';
 
 const Sidebar = ({children}) => {
+  //Context
+  const {setSelectedTodo} = useContext(TodoContext);
+
+  //Ref
+  const sidebarRef = useRef();
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick)
+
+    return () => document.removeEventListener('click', handleClick)
+  })
+
+  const handleClick = e => {  //checks if sidebar and it sorrunding is clicked,then hide EditTodo//
+    if(e.target === sidebarRef.current || sidebarRef.current.contains(e.target)){
+      setSelectedTodo(undefined)
+    }
+  }
+
+
   return (
-    <div className = 'sidebar'>
+    <div 
+      className = 'sidebar'
+      ref={sidebarRef}
+    >
       {children}
     </div>
   )

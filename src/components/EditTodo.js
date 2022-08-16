4aceea912cbcf39ada9457e430a58f2ea9ea17b2
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-16 07:02:22 +0100

    set my selectedTodo and projects from context

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,42 +1,47 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import TodoForm from './TodoForm';
+import {TodoContext} from '../context'
 
 const EditTodo = () => {
-
+  //State
   const [text,setText] = useState();
   const [day,setDay] = useState();
   const [time,setTime] = useState();
   const [todoProject, setTodoProject] = useState();
 
-  const projects = [
-    {id:1, name: 'personal', numOfTodos: 0},
-    {id:2, name: 'work', numOfTodos: 1},
-    {id:3, name: 'other', numOfTodos: 2}
-  ]
+  //Context
+  const {selectedTodo, projects} = useContext(TodoContext)
+  
+
 
   const handleSubmit = (e) => {
 
   }
 
   return (
-    <div className='editTodo'>
-      <div className='header'>
-        EditTodo
-      </div>
-      <div className='container'>
-        <TodoForm 
-          handleSubmit={handleSubmit}
-          text={text}
-          setText={setText}
-          day={day}
-          setDay={setDay}
-          time={time}
-          todoProject={todoProject}
-          setTodoProject={setTodoProject}
-          setTime={setTime}
-          projects={projects}
-        />
-      </div>
+    <div>
+      {
+        selectedTodo &&
+        <div className='editTodo'>
+          <div className='header'>
+            EditTodo
+          </div>
+          <div className='container'>
+            <TodoForm 
+              handleSubmit={handleSubmit}
+              text={text}
+              setText={setText}
+              day={day}
+              setDay={setDay}
+              time={time}
+              todoProject={todoProject}
+              setTodoProject={setTodoProject}
+              setTime={setTime}
+              projects={projects}
+            />
+          </div>
+    </div>
+      }
     </div>
   )
 }

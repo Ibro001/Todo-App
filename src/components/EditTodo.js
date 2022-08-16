dfdc012fc879371ee9dee38474075ceedb6bf226
Ibro001 <ibrahimmoses56@gmail.com>
2022-08-16 07:39:32 +0100

    made the EditTodo functional i can edit a todo and see the change real time

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,18 +1,42 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import TodoForm from './TodoForm';
 import {TodoContext} from '../context'
-
+import moment from 'moment';
+import firebase from '../firebase';
 const EditTodo = () => {
   //State
   const [text,setText] = useState();
-  const [day,setDay] = useState();
-  const [time,setTime] = useState();
-  const [todoProject, setTodoProject] = useState();
+  const [day,setDay] = useState(new Date());
+  const [time,setTime] = useState(new Date());
+  const [todoProject, setTodoProject] = useState('');
 
   //Context
   const {selectedTodo, projects} = useContext(TodoContext)
   
+  useEffect(() => {
+    if(selectedTodo){
+      setText(selectedTodo.text)
+      setDay(moment(selectedTodo.date, 'MM/DD/YYYY'))
+      setTime(moment(selectedTodo.time, 'hh:mm A'))
+      setTodoProject(selectedTodo.projectName)
+    }
+  },[selectedTodo]);
 
+  useEffect(() => {
+    if (selectedTodo){
+      firebase
+      .firestore()
+      .collection('todos')
+      .doc(selectedTodo.id)
+      .update({
+        text,
+        date: moment(day).format('MM/DD/YYYY'),
+        time: moment(time).format('hh:mm A'),
+        day: moment(day).format('d'),
+        projectName: todoProject
+      })
+    }
+  },[time,text,day,todoProject])
 
   const handleSubmit = (e) => {
 
